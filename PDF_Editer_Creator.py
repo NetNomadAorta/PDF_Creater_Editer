72b78fa585a9c7c8bff052b9cd93a53d65117c97
NetNomadAorta <30355837+NetNomadAorta@users.noreply.github.com>
2022-07-24 22:11:06 -0400

    k

--- PDF_Editer_Creator.py
+++ PDF_Editer_Creator.py
@@ -63,18 +63,20 @@ for company in company_list:
         to_append = np.array([[company, company+'.com', position]])
         company_info_matrix = np.append(company_info_matrix, to_append, axis=0)
 
-np.random.shuffle(company_info_matrix)
+
+
 company_info_matrix = np.delete(company_info_matrix, 0, axis=0)
+np.random.shuffle(company_info_matrix)
 
 
-date_to_start = date(2021, 4, 17)
+date_to_start = date(2020, 4, 17)
 date_now = date_to_start + timedelta(days=7)
 
-for pdf in range(1):
-    file_explorer_icon_x = 1465
-    file_explorer_icon_y = 940
+for index in range(9):
+    # file_explorer_icon_x = 1440
+    # file_explorer_icon_y = 940
     
-    click(file_explorer_icon_x, file_explorer_icon_y)
+    # click(file_explorer_icon_x, file_explorer_icon_y)
     
     file_x = 1000
     file_y = 570
@@ -84,15 +86,15 @@ for pdf in range(1):
     time.sleep(1)
     
     date_location_x = 1020
-    date_location_y = 275
+    date_location_y = 275-55
     click(date_location_x, date_location_y)
     typeThis(date_now.strftime("%m/%d/%y"))
     
     cell_x = 520
-    cell_y = 570
+    cell_y = 570-55
     
     for row in range(4):
-        date_row = date_to_start - timedelta(days=5) + timedelta(days=row)
+        date_row = date_now - timedelta(days=5) + timedelta(days=row)
         for col in range(3):
             click(cell_x+150*col, cell_y+30*row)
             time.sleep(0.25)
@@ -105,45 +107,25 @@ for pdf in range(1):
                 time.sleep(0.25)
         
     # Saves file
-    click(1580, 100)
+    # click(1580, 100)
+    # -----------
+    click(20, 40)
+    click(20, 135)
+    # -----------
     time.sleep(0.25)
     typeThis("UB-106-A--" + str(date_now) + ".pdf")
+    # # Click save
+    # click(1000, 660)
     # Click save
-    click(1000, 660)
-    # Click exit
-    click(1680, 20)
+    click(525, 410)
+    # # Click exit
+    # click(1680, 20)
+    click(1680, 15)
+    time.sleep(0.5)
     
-    
-    
-    
-    
+    for ii in range(4):
+        company_info_matrix = np.delete(company_info_matrix, 0, axis=0)
     
     date_now = date_now + timedelta(days=7)
-
-
-
-
-
-
-
-
-
-# Date Section
-# -----------------------------------------------------------------------------
-# 04/24/2021
-date_to_start = date(2021, 4, 17)
-date_now = date_to_start + timedelta(days=7)
-date_now = date_now + timedelta(days=7)
-
-
-
-
-
-
-
-
-
-
-
 
 
